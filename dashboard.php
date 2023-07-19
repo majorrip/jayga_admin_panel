9e75d904755ac6d2985c0e81275ccf7a9a574b6b
Atif_Karim <majorrip0@gmail.com>
2023-07-19 11:44:14 +0600

    mini update

--- dashboard.php
+++ dashboard.php
@@ -123,7 +123,7 @@ if(empty($_SESSION['admin_id'])) {
 			<div class="sidebar-inner slimscroll">
 				<div id="sidebar-menu" class="sidebar-menu">
 					<ul>
-						<li class="active"> <a href="index.php"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a> </li>
+						<li class="active"> <a href="dashboard.php"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a> </li>
 						<li class="list-divider"></li>
 						<li class="submenu"> <a href="#"><i class="fas fa-suitcase"></i> <span> Booking </span> <span class="menu-arrow"></span></a>
 							<ul class="submenu_class" style="display: none;">
