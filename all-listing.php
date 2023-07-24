81e1fbc2f6c2a117a4051102a610baca578916f1
Atif_Karim <majorrip0@gmail.com>
2023-07-25 04:23:49 +0600

    25/7/23
    added data reading

--- all-listing.php
+++ all-listing.php
@@ -1,4 +1,24 @@
-﻿<!DOCTYPE html>
+﻿<?php
+// Replace these variables with your actual database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jayga_db_1";
+// Create a connection to the database
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if (mysqli_connect_errno()) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+// Query to fetch data from the "listing" table
+$query = "SELECT * FROM listing";
+$result = mysqli_query($connection, $query);
+
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -18,7 +38,7 @@
 	<div class="main-wrapper">
 		<div class="header">
 			<div class="header-left">
-				<a href="index.php" class="logo"> <img src="assets/img/hotel_logo.png" width="50" height="70" alt="logo"> <span class="logoclass">HOTEL</span> </a>
+				<a href="index.php" class="logo"> <img src="assets/img/hotel_logo.png" width="50" height="70" alt="logo"> <span class="logoclass">Jayga Admin</span> </a>
 				<a href="index.php" class="logo logo-small"> <img src="assets/img/hotel_logo.png" alt="Logo" width="30" height="30"> </a>
 			</div>
 			<a href="javascript:void(0);" id="toggle_btn"> <i class="fe fe-text-align-left"></i> </a>
@@ -26,7 +46,7 @@
 			<ul class="nav user-menu">
 				<li class="nav-item dropdown noti-dropdown">
 					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"> <i class="fe fe-bell"></i> <span class="badge badge-pill">3</span> </a>
-					<div class="dropdown-menu notifications">
+					<!-- <div class="dropdown-menu notifications">
 						<div class="topnav-dropdown-header"> <span class="notification-title">Notifications</span> <a href="javascript:void(0)" class="clear-noti"> Clear All </a> </div>
 						<div class="noti-content">
 							<ul class="notification-list">
@@ -85,7 +105,7 @@
 							</ul>
 						</div>
 						<div class="topnav-dropdown-footer"> <a href="#">View all Notifications</a> </div>
-					</div>
+					</div> -->
 				</li>
 				<li class="nav-item dropdown has-arrow">
 					<a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"> <span class="user-img"><img class="rounded-circle" src="assets/img/profiles/avatar-01.jpg" width="31" alt="Soeng Souy"></span> </a>
@@ -93,8 +113,8 @@
 						<div class="user-header">
 							<div class="avatar avatar-sm"> <img src="assets/img/profiles/avatar-01.jpg" alt="User Image" class="avatar-img rounded-circle"> </div>
 							<div class="user-text">
-								<h6>Soeng Souy</h6>
-								<p class="text-muted mb-0">Administrator</p>
+								<h6>Jayga Admin</h6>
+								<p class="text-muted mb-0">AK 47</p>
 							</div>
 						</div> <a class="dropdown-item" href="profile.php">My Profile</a> <a class="dropdown-item" href="settings.php">Account Settings</a> <a class="dropdown-item" href="login.php">Logout</a> </div>
 				</li>
@@ -108,8 +128,8 @@
 					<div class="row align-items-center">
 						<div class="col">
 							<div class="mt-5">
-								<h4 class="card-title float-left mt-2">Appointments</h4>
-                                <a href="add-booking.php" class="btn btn-primary float-right veiwbutton ">Add Booking</a>
+								<h4 class="card-title float-left mt-2">Listings Read Data</h4>
+                                <a href="add-listing.php" class="btn btn-primary float-right veiwbutton ">Add Listing</a>
                             </div>
 						</div>
 					</div>
@@ -122,172 +142,75 @@
 									<table class="datatable table table-stripped table table-hover table-center mb-0">
 										<thead>
 											<tr>
-												<th>Booking ID</th>
-												<th>Name</th>
-												<th>Room Type</th>
-												<th>Total Numbers</th>
-												<th>Date</th>
-												<th>Time</th>
-												<th>Arrival Date</th>
-												<th>Depature Date</th>
-												<th>Email ID</th>
-												<th>Ph.Number</th>
-												<th>Status</th>
+												<th>Listing ID</th>
+												<th>Lister ID</th>
+												<th>Lister Name</th>
+												<th>Guest Number</th>
+												<th>Bed Number</th>
+												<th>Bathroom Number</th>
+												<th>Listing Title</th>
+												<th>Listing Description</th>
+												<th>Price</th>
+												<th>Listing Address</th>
+												<th>Zip Code</th>
+												<th>District</th>
+												<th>Town</th>
+												<th>allow short stays</th>
+												<th>Describe Peacefull</th>
+												<th>Describe unique</th>
+												<th>Describe family friendly</th>
+												<th>describe stylish</th>
+												<th>Describe central</th>
+												<th>Describe spacious</th>
+												<th>Bathroom private</th>
+												<th>Breakfast availability</th>
+												<th>Room Lock</th>
+												<th>Who else might be there</th>
+												<th>Listing type</th>
 												<th class="text-right">Actions</th>
 											</tr>
 										</thead>
 										<tbody>
-											<tr>
-												<td>BKG-0001</td>
-												<td>
-													<h2 class="table-avatar">
-                                                    <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-03.jpg" alt="User Image"></a>
-                                                    <a href="profile.php">Tommy Bernal <span>#0001</span></a>
-                                                    </h2>
-                                                </td>
-												<td>Double</td>
-												<td>10</td>
-												<td>21-03-2020</td>
-												<td>11.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="2652494b4b5f44435448474a66435e474b564a430845494b">[email&#160;protected]</a></td>
-												<td>631-254-6480</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Active</a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>BKG-0002</td>
-												<td>
-													<h2 class="table-avatar">
-                                                        <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-04.jpg" alt="User Image"></a>
-                                                        <a href="profile.php">Richard Brobst <span>#0002</span></a>
-                                                    </h2>
-                                                </td>
-												<td>Single</td>
-												<td>8</td>
-												<td>21-03-2020</td>
-												<td>09.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="fd8f949e959c8f999f8f929f8e89bd98859c908d9198d39e9290">[email&#160;protected]</a></td>
-												<td>830-468-1042</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Inactive</a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>BKG-0003</td>
-												<td>
-													<h2 class="table-avatar">
-                                                    <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-05.jpg" alt="User Image"></a>
-                                                    <a href="profile.php">Ellen Thill <span>#0003</span></a>
-                                                    </h2> 
-                                                </td>
-												<td>Double</td>
-												<td>10</td>
-												<td>21-03-2020</td>
-												<td>08.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="b2d7deded7dcc6dadbdedef2d7cad3dfc2ded79cd1dddf">[email&#160;protected]</a></td>
-												<td>508-335-5531</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Active</a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>BKG-0004</td>
-												<td>
-													<h2 class="table-avatar">
-                                                    <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-06.jpg" alt="User Image"></a>
-                                                    <a href="profile.php">Corina Kelsey <span>#0004</span></a>
-                                                    </h2>
-                                                </td>
-												<td>Single</td>
-												<td>15</td>
-												<td>21-03-2020</td>
-												<td>12.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="781b170a111619131d140b1d01381d00191508141d561b1715">[email&#160;protected]</a></td>
-												<td>262-260-1170</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2"> Active</a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>BKG-0005</td>
-												<td>
-													<h2 class="table-avatar">
-                                                    <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-07.jpg" alt="User Image"></a>
-                                                    <a href="profile.php">Carolyn Lane <span>#0005</span></a>
-                                                    </h2>
-                                                </td>
-												<td>Double</td>
-												<td>12</td>
-												<td>21-03-2020</td>
-												<td>06.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="6c0f0d1e03001502000d02092c09140d011c0009420f0301">[email&#160;protected]</a></td>
-												<td>570-458-0070</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Active</a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>BKG-0006</td>
-												<td>
-													<h2 class="table-avatar">
-                                                        <a href="profile.php" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/profiles/avatar-08.jpg" alt="User Image"></a>
-                                                        <a href="profile.php">David Alvarez <span>#0006</span></a>
-                                                    </h2>
-                                                </td>
-												<td>Single</td>
-												<td>7</td>
-												<td>21-03-2020</td>
-												<td>10.00 AM</td>
-												<td>22-03-2020</td>
-												<td>23-03-2020</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="fb9f9a8d929f9a978d9a899e81bb9e839a968b979ed5989496">[email&#160;protected]</a></td>
-												<td>212-414-9510</td>
-												<td>
-													<div class="actions"> <a href="#" class="btn btn-sm bg-success-light mr-2">Inactive </a> </div>
-												</td>
-												<td class="text-right">
-													<div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-														<div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-booking.php"><i class="fas fa-pencil-alt m-r-5"></i> Edit</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Delete</a> </div>
-													</div>
-												</td>
-											</tr>
-										</tbody>
+            <?php
+            // Check if there are rows returned from the query
+            if ($result->num_rows > 0) {
+                // Loop through the rows and print data in the table
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["listing_id"] . "</td>";
+                    echo "<td>" . $row["lister_id"] . "</td>";
+                    echo "<td>" . $row["lister_name"] . "</td>";
+                    echo "<td>" . $row["guest_num"] . "</td>";
+                    echo "<td>" . $row["bed_num"] . "</td>";
+                    echo "<td>" . $row["bathroom_num"] . "</td>";
+                    echo "<td>" . $row["listing_title"] . "</td>";
+                    echo "<td>" . $row["listing_description"] . "</td>";
+                    echo "<td>" . $row["full_day_price_set_by_user"] . "</td>";
+                    echo "<td>" . $row["listing_address"] . "</td>";
+                    echo "<td>" . $row["zip_code"] . "</td>";
+                    echo "<td>" . $row["district"] . "</td>";
+                    echo "<td>" . $row["town"] . "</td>";
+                    echo "<td>" . $row["allow_short_stay"] . "</td>";
+                    echo "<td>" . $row["describe_peaceful"] . "</td>";
+                    echo "<td>" . $row["describe_unique"] . "</td>";
+                    echo "<td>" . $row["describe_familyfriendly"] . "</td>";
+                    echo "<td>" . $row["describe_stylish"] . "</td>";
+                    echo "<td>" . $row["describe_central"] . "</td>";
+                    echo "<td>" . $row["describe_spacious"] . "</td>";
+                    echo "<td>" . $row["bathroom_private"] . "</td>";
+                    echo "<td>" . $row["breakfast_availability"] . "</td>";
+                    echo "<td>" . $row["room_lock"] . "</td>";
+                    echo "<td>" . $row["who_else_might_be_there"] . "</td>";
+                    echo "<td>" . $row["listing_type"] . "</td>";
+                    echo '<td class="text-right"><a href="#">Edit</a> | <a href="#">Delete</a></td>';
+                    echo "</tr>";
+                }
+            } else {
+                // If no data is found, display a message in a single row
+                echo '<tr><td colspan="26">No data found.</td></tr>';
+            }
+            ?>
+        </tbody>
 									</table>
 								</div>
 							</div>
