28b78296c549e28f3d1801cc56d50c832cf59797
Atif_Karim <majorrip0@gmail.com>
2023-08-05 04:12:08 +0600

    5/8/23
    api work

--- API/log-in/login-api.php
+++ API/log-in/login-api.php
@@ -1,31 +1,31 @@
 <?php
 
-// Function to generate a random token
-function generateToken($length = 10) {
-    $characters = '0123456789';
-    $token = '';
-    for ($i = 0; $i < $length; $i++) {
-        $token .= $characters[rand(0, strlen($characters) - 1)];
-    }
-    return $token;
+// Function to generate a random 4-digit OTP
+function generateOTP() {
+    return strval(rand(1000, 9999));
 }
 
-// Function to generate a random user ID
-function generateRandomUserID() {
-    // Generate a random 4-digit number
-    $randomNumber = str_pad(rand(0, 9999), 4, '0', STR_PAD_LEFT);
-    
-    // Get the current date and time in the format: YYYYMMDD_HHMMSS
-    $dateTime = date('Ymd_His');
-
-    // Combine the random number and date-time to create the user ID
-    $userID = $dateTime . '_' . $randomNumber;
-
-    return $userID;
+// Function to generate a random bearer token
+function generateBearerToken() {
+    return bin2hex(random_bytes(32)); // Generate a 32-byte random string as a token
 }
 
-// Assuming you have a function to connect to the database
-function connectToDatabase() {
+// Function to hash the OTP securely
+function hashOTP($otp) {
+    return password_hash($otp, PASSWORD_BCRYPT);
+}
+
+// Function to validate the Bearer Token
+function isValidBearerToken($bearerToken) {
+    // Implement your validation logic here
+    // For example, check if the bearer token exists in the database and is associated with a valid user
+    // This is a placeholder, and you should replace it with your own validation logic
+    return !empty($bearerToken);
+}
+
+// Function to fetch user data by phone number from the database
+function fetchUserByPhoneNumber($phoneNumber) {
+    // Implement your database connection logic here
     $host = 'your_host';
     $username = 'your_username';
     $password = 'your_password';
@@ -34,50 +34,129 @@ function connectToDatabase() {
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch (PDOException $e) {
-        // Handle connection error
-        die("Connection failed: " . $e->getMessage());
-    }
-}
 
-// API endpoint for login or generating token
-function loginOrGenerateToken() {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Assuming the phone number is sent in the request body as a JSON field called 'phone_number'
-        $data = json_decode(file_get_contents('php://input'), true);
-        $phoneNumber = $data['phone_number'];
-
-        // Connect to the database
-        $conn = connectToDatabase();
-
-        // Check if the phone number exists in the database
+        // Fetch the user data from the database based on the phone number
         $stmt = $conn->prepare('SELECT * FROM users WHERE phone_number = :phone_number');
         $stmt->bindParam(':phone_number', $phoneNumber);
         $stmt->execute();
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($user) {
-            // If the phone number exists, you can log the user in here
-            // Do whatever you need to do for login functionality
-            $response = array('message' => 'Logged in successfully');
-        } else {
-            // If the phone number doesn't exist, generate a random token and user ID and save them to the database
-            $token = generateToken();
-            $userID = generateRandomUserID();
-            $stmt = $conn->prepare('INSERT INTO users (user_id, phone_number, token) VALUES (:user_id, :phone_number, :token)');
-            $stmt->bindParam(':user_id', $userID);
-            $stmt->bindParam(':phone_number', $phoneNumber);
-            $stmt->bindParam(':token', $token);
-            $stmt->execute();
-            $response = array('message' => 'User ID and Token generated and saved');
-        }
-
-        // Send the response back to the client
-        header('Content-Type: application/json');
-        echo json_encode($response);
+        return $user;
+    } catch (PDOException $e) {
+        // Handle database connection error
+        header('HTTP/1.1 500 Internal Server Error');
+        echo json_encode(array('error' => 'Internal Server Error'));
+        die();
     }
 }
 
-// Call the API function
-loginOrGenerateToken();
+// API endpoint for user registration and login
+function userRegistrationAndLogin() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Assuming the phone number is sent in the request body as a JSON field called 'phone_number'
+        $data = json_decode(file_get_contents('php://input'), true);
+        $phoneNumber = $data['phone_number'];
+
+        // Generate a random 4-digit OTP
+        $otp = generateOTP();
+
+        // Securely hash the OTP before saving to the database
+        $hashedOtp = hashOTP($otp);
+
+        // Generate a more secure bearer token
+        $bearerToken = generateBearerToken();
+
+        // Implement your database connection logic here
+        $host = 'your_host';
+        $username = 'your_username';
+        $password = 'your_password';
+        $dbname = 'your_database_name';
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Save the user data to the database
+            $stmt = $conn->prepare('INSERT INTO users (phone_number, otp, bearer_token) VALUES (:phone_number, :otp, :bearer_token)');
+            $stmt->bindParam(':phone_number', $phoneNumber);
+            $stmt->bindParam(':otp', $hashedOtp);
+            $stmt->bindParam(':bearer_token', $bearerToken);
+            $stmt->execute();
+
+            // Send the OTP to the user via SMS or any other method
+            // You can implement the SMS sending logic here
+
+            // Return the bearer token and actual OTP to the user (for demonstration purposes)
+            header('Content-Type: application/json');
+            echo json_encode(array(
+                'bearer_token' => $bearerToken,
+                'otp' => $otp
+            ));
+        } catch (PDOException $e) {
+            // Handle database connection error
+            header('HTTP/1.1 500 Internal Server Error');
+            echo json_encode(array('error' => 'Internal Server Error'));
+            die();
+        }
+    }
+}
+
+// API endpoint for login
+function userLogin() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Assuming the phone number, OTP, and bearer token are sent in the request body as JSON fields
+        $data = json_decode(file_get_contents('php://input'), true);
+        $phoneNumber = $data['phone_number'];
+        $otp = $data['otp'];
+        $bearerToken = $data['bearer_token'];
+
+        // Implement your database connection logic here
+        $host = 'your_host';
+        $username = 'your_username';
+        $password = 'your_password';
+        $dbname = 'your_database_name';
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Fetch the user data from the database based on the phone number
+            $user = fetchUserByPhoneNumber($phoneNumber);
+
+            if ($user && password_verify($otp, $user['otp']) && isValidBearerToken($bearerToken)) {
+                // If the phone number, OTP, and bearer token match, return the user ID and phone number
+                header('Content-Type: application/json');
+                echo json_encode(array(
+                    'status' => 'OK',
+                    'access_token' => $bearerToken,
+                    'token_type' => 'bearer',
+                    'message' => 'You have successfully logged in',
+                    'user' => array(
+                        'id' => $user['id'],
+                        'phone' => $user['phone'],
+                        'otp' => $otp
+                    )
+                ));
+            } else {
+                // If validation fails, return an error response
+                header('HTTP/1.1 401 Unauthorized');
+                echo json_encode(array('error' => 'Unauthorized'));
+            }
+        } catch (PDOException $e) {
+            // Handle database connection error
+            header('HTTP/1.1 500 Internal Server Error');
+            echo json_encode(array('error' => 'Internal Server Error'));
+            die();
+        }
+    }
+}
+
+// Call the API function based on the endpoint (registration or login)
+if ($_SERVER['REQUEST_URI'] === '/userRegistrationAndLogin') {
+    userRegistrationAndLogin();
+} elseif ($_SERVER['REQUEST_URI'] === '/userLogin') {
+    userLogin();
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo json_encode(array('error' => 'Not Found'));
+}
