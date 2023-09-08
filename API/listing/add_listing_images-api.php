2cd9a3b5a7bd26a5fd1286f8141ec698c89cd742
Atif_Karim <majorrip0@gmail.com>
2023-09-09 01:46:29 +0600

    9/8/23
    listing image final

--- API/listing/add_listing_images-api.php
+++ API/listing/add_listing_images-api.php
@@ -18,16 +18,14 @@ try {
     die();
 }
 
+// Initialize arrays to store error and success messages
+$errors = array();
+$success = false; // Initialize a success flag
+
 // Handle RESTful API routing
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $listing_id = $_POST['listing_id'];
     $lister_id = $_POST['lister_id'];
-
-    // Initialize an object to store data
-    $listing_data = new stdClass();
-    $listing_data->listing_id = $listing_id;
-    $listing_data->lister_id = $lister_id;
-    $listing_data->listing_images = array();
 
     // Check if files were uploaded
     if (!empty($_FILES['listing_pictures']['name'][0])) {
@@ -36,18 +34,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $file_tmp = $_FILES['listing_pictures']['tmp_name'][$i];
             $file_extension = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-            $allowed_extensions = array('jpg', 'jpeg', 'png');
+            $allowed_extensions = array('jpg', 'jpeg', 'png', 'bmp');
             if (in_array($file_extension, $allowed_extensions)) {
-                // Debugging
-                // echo "Uploading file: " . $file_name . "<br>";
-
-                // Read the file into a base64-encoded string
-                $file_data = base64_encode(file_get_contents($file_tmp));
-
-                // Debugging
-                // echo "Base64-encoded file data:<br>";
-                // echo $file_data . "<br>";
-
                 // Generate a unique filename
                 $file_name = $listing_id . "-" . $lister_id . "_listing-pic_" . date("YmdHis") . "_" . rand(1000, 9999) . "." . $file_extension;
 
@@ -60,35 +48,42 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 // Save the image to the server
                 $target_file = $listing_folder . $file_name;
-                if (file_put_contents($target_file, base64_decode($file_data))) {
-                    // Add the base64-encoded image data to the listing data
-                    $listing_data->listing_images[] = array(
-                        'listing_picture' => $file_name,
-                        'listing_picture_base64' => $file_data
-                    );
-                    
+                if (move_uploaded_file($file_tmp, $target_file)) {
                     // Insert record into the database
                     $sql = "INSERT INTO listing_images (listing_id, lister_id, listing_filename, listing_targetlocation) VALUES (?, ?, ?, ?)";
                     $stmt = $conn->prepare($sql);
                     $stmt->execute([$listing_id, $lister_id, $file_name, $target_file]);
                     $stmt->closeCursor();
+
+                    // Set the success flag to true
+                    $success = true;
                 } else {
-                    echo json_encode(array('error' => 'Error saving the image.')) . "<br>";
+                    // Add error message to the array
+                    $errors[] = "Error saving the image '$file_name'.";
                 }
             } else {
-                echo json_encode(array('error' => 'Invalid image format for file: ' . $file_name)) . "<br>";
+                // Add error message to the array
+                $errors[] = "Invalid image format for file: $file_name";
             }
         }
     } else {
-        echo json_encode(array('error' => 'No files were uploaded.')) . "<br>";
+        // Add error message to the array
+        $errors[] = "No files were uploaded.";
     }
 
-    // After processing all uploaded files, return the listing data in JSON format
-    if (!empty($listing_data->listing_images)) {
-        echo json_encode($listing_data) . "<br>";
+    // Prepare the response data
+    $response = array();
+    if (!empty($errors)) {
+        $response['error'] = implode("\n", $errors);
     }
+    if ($success) {
+        $response['message'] = "Image uploaded successfully.";
+    }
+
+    // Send a single JSON response with both error and success messages
+    echo json_encode($response);
 } else {
     header('HTTP/1.1 404 Not Found');
-    echo json_encode(array('error' => 'Not Found')) . "<br>";
+    echo json_encode(array('error' => 'Not Found'));
 }
 ?>
